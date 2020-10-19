d6d2d58a95da6c9b8ad8fbe04fb5b4b55df10d48
ItsDandelia <69596233+ItsDandelia@users.noreply.github.com>
2020-10-19 15:07:02 +0530

    Create Jatin Dandelia_D14A.py

---
+++ Jatin Dandelia_D14A.py
@@ -0,0 +1,17 @@
+# Copy your code here
+
+from selenium import webdriver
+import Passwords
+import time
+from selenium.webdriver.chrome.options import Options
+
+opt=Options()
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+# Pass the argument 1 to allow and 2 to block
+opt.add_experimental_option("prefs", { \
+"profile.default_content_setting_values.media_stream_mic": 1,
+"profile.default_content_setting_values.media_stream_camera": 1,
+"profile.default_content_setting_values.geolocation": 1,
+"profile.default_content_setting_values.notifications": 1
+})
